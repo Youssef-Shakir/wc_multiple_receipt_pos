18cf5cbf449c972caafbeafff2a9912d6ddd0cbd
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-18 06:14:14 +0300

    d

--- static/src/js/payment_screen.js
+++ static/src/js/payment_screen.js
@@ -5,14 +5,15 @@ import { PosStore } from "@point_of_sale/app/store/pos_store";
 
 patch(PosStore.prototype, {
     async printReceipt(options = {}) {
-        // Check if multi-receipt is enabled and there's a valid current order
-        const currentOrder = this.get_order();
-        const canMultiPrint = this.config.multi_receipt_print && currentOrder;
-        const printCount = canMultiPrint ? (this.config.receipt_print_time || 1) : 1;
+        // Always do the first print
+        const result = await super.printReceipt(options);
 
-        let result;
-        for (let i = 0; i < printCount; i++) {
-            result = await super.printReceipt(options);
+        // Check if multi-receipt is enabled for additional prints
+        if (this.config.multi_receipt_print && this.config.receipt_print_time > 1) {
+            const additionalPrints = this.config.receipt_print_time - 1;
+            for (let i = 0; i < additionalPrints; i++) {
+                await super.printReceipt(options);
+            }
         }
 
         return result;
