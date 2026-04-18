23ebd69accc4a3b513db88bb12c41d7a2e9127ab
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-18 05:35:09 +0300

    fix

--- static/src/js/payment_screen.js
+++ static/src/js/payment_screen.js
@@ -1,51 +1,20 @@
 /** @odoo-module */
 
 import { patch } from "@web/core/utils/patch";
-import { range } from "@web/core/utils/numbers";
-import { _t } from "@web/core/l10n/translation";
-import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
 
-//
 patch(PosStore.prototype, {
-
-
     async printReceipt({
-    basic = false,
-    order = this.get_order(),
-    printBillActionTriggered = false,
+        basic = false,
+        order = this.get_order(),
+        printBillActionTriggered = false,
     } = {}) {
-        if (this.config.multi_receipt_print){
-            this.new_temp_variable = false
-            for (var print_time of range(0,this.config.receipt_print_time)) {
-                const result = await this.printer.print(
-                OrderReceipt,
-                {
-                    data: this.orderExportForPrinting(order),
-                    formatCurrency: this.env.utils.formatCurrency,
-                    basic_receipt: basic,
-                },
-                { webPrintFallback: true }
-                );
-                this.new_temp_variable = result
-            }
-            setTimeout(async() =>{
-                if (this.new_temp_variable && !printBillActionTriggered) {
-                    order.nb_print = order.nb_print ? order.nb_print + 1 : 1;
-                    if (typeof order.id === "number" && this.new_temp_variable) {
-                        await this.data.write("pos.order", [order.id], { nb_print: order.nb_print });
-                    }
-                } else if (!order.nb_print) {
-                    order.nb_print = 0;
-                }
-                if (this.new_temp_variable?.warningCode) {
-                    this.displayPrinterWarning(this.new_temp_variable, _t("Receipt Printer"));
-                }
-                return this.new_temp_variable;
-            })
+        let result = true;
+        const printCount = this.config.multi_receipt_print ? (this.config.receipt_print_time || 1) : 1;
 
-       }else{
-            const result = await this.printer.print(
+        for (let i = 0; i < printCount; i++) {
+            result = await this.printer.print(
                 OrderReceipt,
                 {
                     data: this.orderExportForPrinting(order),
@@ -54,20 +23,14 @@ patch(PosStore.prototype, {
                 },
                 { webPrintFallback: true }
             );
-            if (!printBillActionTriggered) {
-                order.nb_print = order.nb_print ? order.nb_print + 1 : 1;
-                if (typeof order.id === "number" && result) {
-                    await this.data.write("pos.order", [order.id], { nb_print: order.nb_print });
-                }
-            } else if (!order.nb_print) {
-                order.nb_print = 0;
-            }
-            if (result?.warningCode) {
-                this.displayPrinterWarning(result, _t("Receipt Printer"));
-            }
-            return result;
+        }
 
-       }
-
+        if (!printBillActionTriggered) {
+            order.nb_print += 1;
+            if (typeof order.id === "number" && result) {
+                await this.data.write("pos.order", [order.id], { nb_print: order.nb_print });
+            }
+        }
+        return true;
     }
 });
