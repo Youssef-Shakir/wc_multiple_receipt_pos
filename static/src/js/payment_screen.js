1e0c282a4310f5c575c23a0bd62b2ecfe9df4d46
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-18 05:43:09 +0300

    fix

--- static/src/js/payment_screen.js
+++ static/src/js/payment_screen.js
@@ -7,20 +7,32 @@ import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/
 patch(PosStore.prototype, {
     async printReceipt({
         basic = false,
-        order = this.get_order(),
+        order,
         printBillActionTriggered = false,
     } = {}) {
+        // Always get current order if not provided or undefined
+        if (!order) {
+            order = this.get_order();
+        }
+        if (!order) {
+            console.error('[MultiReceipt] No order available for printing');
+            return false;
+        }
+
         let result = true;
         const printCount = this.config.multi_receipt_print ? (this.config.receipt_print_time || 1) : 1;
 
         for (let i = 0; i < printCount; i++) {
+            // Get fresh print data for each iteration
+            const printData = {
+                data: this.orderExportForPrinting(order),
+                formatCurrency: this.env.utils.formatCurrency,
+                basic_receipt: basic,
+            };
+
             result = await this.printer.print(
                 OrderReceipt,
-                {
-                    data: this.orderExportForPrinting(order),
-                    formatCurrency: this.env.utils.formatCurrency,
-                    basic_receipt: basic,
-                },
+                printData,
                 { webPrintFallback: true }
             );
         }
