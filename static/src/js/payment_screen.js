b7daa95a4ebd011cd0e4a900dad842c47f510af7
Youssef-Shakir <youssef.shakir.2003@gmail.com>
2026-04-18 05:48:46 +0300

    fix

--- static/src/js/payment_screen.js
+++ static/src/js/payment_screen.js
@@ -2,47 +2,17 @@
 
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
-import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
 
 patch(PosStore.prototype, {
-    async printReceipt({
-        basic = false,
-        order,
-        printBillActionTriggered = false,
-    } = {}) {
-        // Always get current order if not provided or undefined
-        if (!order) {
-            order = this.get_order();
-        }
-        if (!order) {
-            console.error('[MultiReceipt] No order available for printing');
-            return false;
-        }
-
-        let result = true;
+    async printReceipt(options = {}) {
         const printCount = this.config.multi_receipt_print ? (this.config.receipt_print_time || 1) : 1;
 
+        let result;
         for (let i = 0; i < printCount; i++) {
-            // Get fresh print data for each iteration
-            const printData = {
-                data: this.orderExportForPrinting(order),
-                formatCurrency: this.env.utils.formatCurrency,
-                basic_receipt: basic,
-            };
-
-            result = await this.printer.print(
-                OrderReceipt,
-                printData,
-                { webPrintFallback: true }
-            );
+            // Call the original printReceipt method
+            result = await super.printReceipt(options);
         }
 
-        if (!printBillActionTriggered) {
-            order.nb_print += 1;
-            if (typeof order.id === "number" && result) {
-                await this.data.write("pos.order", [order.id], { nb_print: order.nb_print });
-            }
-        }
-        return true;
+        return result;
     }
 });
